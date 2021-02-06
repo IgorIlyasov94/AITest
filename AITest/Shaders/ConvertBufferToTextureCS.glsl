5aa32475c24bf928e2c906dc7aa933f6fd43b99d
i.ilyasov <igorilyasov94@gmail.com>
2021-02-06 04:45:26 +0300

    Add readonly/writeonly

--- AITest/Shaders/ConvertBufferToTextureCS.glsl
+++ AITest/Shaders/ConvertBufferToTextureCS.glsl
@@ -5,12 +5,12 @@ layout(local_size_x = 1) in;
 uniform int imageWidth;
 uniform int imageHeight;
 
-layout(std430, binding = 0) buffer inputBuffer
+layout(std430, binding = 0) readonly buffer inputBuffer
 {
 	vec4 textureData[];
 };
 
-layout(rgba8, binding = 1) uniform image2D outputTexture;
+layout(rgba8, binding = 1) writeonly uniform image2D outputTexture;
 
 void main()
 {
--- AITest/Shaders/ConvertTextureToBufferCS.glsl
+++ AITest/Shaders/ConvertTextureToBufferCS.glsl
@@ -5,9 +5,9 @@ layout(local_size_x = 1, local_size_y = 1) in;
 uniform int imageWidth;
 uniform int imageHeight;
 
-layout(rgba8, binding = 0) uniform image2D inputTexture;
+layout(rgba8, binding = 0) readonly uniform image2D inputTexture;
 
-layout(std430, binding = 1) buffer outputBuffer
+layout(std430, binding = 1) writeonly buffer outputBuffer
 {
 	vec4 textureData[];
 };
